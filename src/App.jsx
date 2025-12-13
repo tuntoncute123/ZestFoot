3d6673903280c6e5558efb7ddbf4816344e25228
Chi Khang <22130112@st.hcmuaf.edu.vn>
2025-12-13 19:01:20 +0700

    Them chuc nang forgot pw

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/Home/HomePage';
 import Login from './components/LogIn_SignUp/Login';
 import SignUp from './components/LogIn_SignUp/SignUp';
+import ForgotPassword from './components/LogIn_SignUp/ForgotPassword';
 
 function App() {
   return (
@@ -14,6 +16,7 @@ function App() {
           <Route path="/cart" element={<div style={{ padding: '100px', textAlign: 'center' }}>Cart Page Placeholder</div>} />
             <Route path="/register" element={<SignUp />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/forgot" element={<ForgotPassword />} />
         </Routes>
       </div>
     </Router>
